74b08acf74125b9a95c109c7b4e60b48ebb10155
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2023-06-13 10:02:41 -0400

    Separando frase por coma y espacio

--- src/Palabras.js
+++ src/Palabras.js
@@ -1,12 +1,17 @@
 function get_frase(frase){
     return frase;
 }
-function separar_frase(frase){
+function separar_fraseComa(frase){
     const Frase = get_frase(frase);
     var palabras = []
-    const separadores = [' ', ',','-',';','.'];
+    palabras = Frase.split(',');
+    return palabras;
+}
+function separar_fraseEspacio(frase){
+    const Frase = get_frase(frase);
+    var palabras = []
     palabras = Frase.split(' ');
     return palabras;
 }
 
-export {get_frase, separar_frase};
+export {get_frase, separar_fraseComa, separar_fraseEspacio};
--- src/Palabras.test.js
+++ src/Palabras.test.js
@@ -1,10 +1,13 @@
-import {get_frase, separar_frase} from "./Palabras";
+import {get_frase, separar_fraseComa, separar_fraseEspacio} from "./Palabras";
 
 describe("Calcular Palabras", () => {
     it("Deberia retornar la frase que se esta utilizando", () => {
       expect(get_frase("Hola")).toEqual("Hola");
     });
     it("Deberia retornar la frase que se esta utilizando separada", () => {
-      expect(separar_frase("hola Que tal")).toEqual(["hola", "Que", "tal"]);
+      expect(separar_fraseEspacio("hola Que tal")).toEqual(["hola","Que","tal"]);
+    });
+    it("Deberia retornar la frase que se esta utilizando separada por una coma", () => {
+      expect(separar_fraseComa("hola,Que,tal")).toEqual(["hola", "Que", "tal"]);
     });
 });
